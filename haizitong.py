2d9a861783f235250b5e605e4ed5dcede796b295
liwei <liwei@zhongan.io>
2021-07-08 13:03:37 +0800

    修改孩子通

--- haizitong.py
+++ haizitong.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 # API_URL
 account_login_url = "https://hzt-app1.haizitong.com/2/org/account/login"
 user_login_url = "https://hzt-app1.haizitong.com/2/org/mobile/user/login"
-reply_url = "https://hzt-app1.haizitong.com/2/s/survey/question/reply"
+reply_url = "https://hzt-app2.haizitong.com/2/s/survey/question/reply"
 
 # VARIABLE NAME
 DEVICE = os.environ['DEVICE']
@@ -71,7 +71,7 @@ def userLogin(mobileLoginData):
 def reply(userToken):
     if userToken is not None:
         logger.info('开始提交答案请求')
-        answers = '[{"options":[{"optionId":"d3561819-cc60-41b7-884d-1ec3106141d6"}],"itemId":"b4c83fbe-8f77-4154-9f6a-dfa7444aa318"},{"text":"36.5","itemId":"fa13c111-bcf2-4a84-b70b-5a5d9772857c"},{"options":[{"optionId":"d8097d9556f442c0800dcec009da5cc6"}],"itemId":"22c650fd65e54c348fb0d8ce40fe0f5c"},{"options":[{"optionId":"cb07573c-56f0-4ae9-92db-7ed451e5df30"}],"itemId":"303ccb79-a60c-47ca-921b-275f6ced43a6"},{"files":[{"picHeight":1316,"picWidth":794,"url":"http:\/\/min.haizitong.com\/2\/ali\/i\/3086ddf9565f4995b7bb9c203566fb4a","type":"i"}],"itemId":"8c5284e0-4380-40b8-b031-38903aeac459"},{"options":[{"optionId":"3dae73e8e87c4196b151a891593c45c9"}],"itemId":"9bc3dbb0c378405e8f8529639c7942fb"},{"text":"李慰37.2","itemId":"b85ff1f8-e2c5-43a9-a7d4-c88effd19014"},{"options":[{"optionId":"5913424e-adf0-4a3e-8683-aa1f7d151543"}],"itemId":"7cc442c7-2349-4ba7-8233-9c43f922dc2c"},{"files":[{"picHeight":1219,"picWidth":796,"url":"http:\/\/min.haizitong.com\/2\/ali\/i\/45420d840ed64851a6482756238fc8c7","type":"i"}],"itemId":"76674aa4-2ebe-4f4f-937a-383ef0b939fc"},{"files":[{"picHeight":788,"picWidth":1012,"url":"http:\/\/min.haizitong.com\/2\/ali\/i\/aa35461fbf924eaaa360f8a895d9568e","type":"i"}],"itemId":"7dcfb064-5794-42db-a057-2a3bc542a648"}]'
+        answers = '[{"options":[{"optionId":"568ace52-38c9-4312-80ad-7ce3ecc4b09b"}],"itemId":"1445cb71-8b3a-4995-8cb1-f13bf6fb0f20"},{"options":[{"optionId":"e09d9680-98e5-43b2-a59a-b7e4cfdf6f0e"}],"itemId":"9dd3337a-10ca-4f96-96b2-484f2b3da52d"},{"options":[{"optionId":"6bdd4bb4-de08-4455-8be8-6121aac81b2c"}],"itemId":"dee0523d-e4ea-47e2-886e-c8321296f092"},{"options":[{"optionId":"4a834e27-571a-463c-ac86-7e300d65ddef"}],"itemId":"96e92454-a36d-469c-88bd-05e6d1a154de"},{"files":[{"picHeight":1316,"picWidth":794,"url":"https:\/\/min.haizitong.com\/2\/ali\/i\/0087c2cf18c346308636756879ba7e07","type":"i"}],"itemId":"a6781f9f32284a1dac65900c309cc2e9"}]'
         replyRequestData = {'answers': answers,
                             'who': '1',
                             'surveyId': '5f3a5ac3d017dc0867d2b6fe'}
